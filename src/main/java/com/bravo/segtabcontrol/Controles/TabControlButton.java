ae62a949532e8ab83dddab951c8704bd34959c1b
gbravo <germanchobravo@gmail.com>
2014-08-19 10:16:10 -0400

    se agregan los recursos

---
+++ src/main/java/com/bravo/segtabcontrol/Controles/TabControlButton.java
@@ -0,0 +1,17 @@
+package com.bravo.segtabcontrol.Controles;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import com.bravo.segtabcontrol.R;
+
+/**
+ * Created by gbravo on 7/24/14.
+ */
+public class TabControlButton extends SegmentedControlButton {
+
+    public TabControlButton(Context context, AttributeSet attrs) {
+        super(context, attrs, R.attr.tabControlButtonStyle);
+    }
+
+}
+
